3af04af7f82d406d6b5986345baf48f45dbbbf93
Karl Clinckspoor <karl@unicamp.br>
2022-06-02 08:23:08 -0300

    Added more tests to SetBits, fixed a bug in its implementation, completed its documentation.

--- Randomizer/Utils.cs
+++ Randomizer/Utils.cs
@@ -5,59 +5,58 @@
     /// </summary>
     public static class Utils
     {
-        // TODO: Don't I have to shift the mask also, in the first step? Seems I'm missing the logic here.
         /// <summary>
         /// Sets specific bits of "newvalue" into "currvalue" obeying a mask and a shift.
         /// Mask acts as the number of bits to be set, shift is its position.
         ///
-        /// Note: Can't have this function with type short inputs because +, <<, etc, are all defined for ints only...
+        /// Taken from https://stackoverflow.com/questions/5925755/how-to-replace-bits-in-a-bitfield-without-affecting-other-bits-using-c
         ///
-        /// Example: Want to modify currvalue (1100 1110) to (1100 0110) by setting the last 4 bits.
+        /// Note: Can't have this function with type short inputs because +, &lt&lt, etc, are all defined for ints only...
+        ///
+        /// Example:
         /// 
-        ///     currvalue: 1100 1110
-        ///     newvalue:  0011 0110
-        ///     mask:      0000 1111
-        ///     shift:     0
+        ///     currvalue: 1100_1110  // What we current have
+        ///     expected:  1101_0110  // Out objective
+        ///     newvalue:  1001_0000  // A set of bits we want to set in currvalue. Note the first bit is ignored
+        ///     mask:      0011_1000  // The bits we want to modify are 1, preserve are 0
+        ///     shift:     0          // If desired, newvalue and mask are left shifted
         ///     
+        /// Step 0: Moves the mask to the desired position. No effect in this example
         /// 
-        ///     
         /// Step 1: Sets the values to be modified to 0. Preserves the rest
-        ///     currvalue & (~mask):
-        ///     1100 1110 &
-        ///     1111 0000
+        ///     currvalue & (~(mask &lt&lt shift))
+        ///     1100_1110 &
+        ///     1100_0111
         ///     ---------
-        ///     1100 0000
+        ///     1100_0110 // Set "to change" bits to 0
         ///     
         /// Step 2: Sets the bits in newvalue that will be copied over.
         ///     newvalue & mask:
-        ///     0011 0110 &
-        ///     0000 1111
+        ///     1001_0000 &
+        ///     0011_1000
         ///     ---------
-        ///     0000 0110
+        ///     0001_0000
         /// 
         /// Step 3: Moves over values to be copied to their intended position.
-        ///     (newvalue & mask) << shift
-        ///     0000 0110 << 0
-        ///     --------------
-        ///     0000 0110
         ///     
-        /// Step 4: ORs the new bits in the current value
-        ///     (currvalue & (~mask)) | ((newvalue & mask) << shift)
-        ///     1100 0000 |
-        ///     0000 0110
+        /// Step 4: ORs the results from Step 1 and Step 2:
+        ///     (Step 1) | (Step 2)
+        ///     1100_0110 |
+        ///     0001_0000
         ///     ---------
-        ///     1100 0110
+        ///     1101_0110  // == expected
         ///     
+        /// Check the UnitTests for more examples
         /// </summary>
-        /// <param name="currvalue"></param>
-        /// <param name="newvalue"></param>
-        /// <param name="mask"></param>
-        /// <param name="shift"></param>
+        /// <param name="currvalue">Current value of the variable where bits will be set</param>
+        /// <param name="newvalue">Variable with a set of bits to change </param>
+        /// <param name="mask">A bitmask that selects which bits from currvalue and newvalue will be considered</param>
+        /// <param name="shift">A number of leftshift operations on the mask. Note this affects the newvalue before applying the mask</param>
         /// <returns></returns>
         public static int SetBits(int currvalue, int newvalue, int mask, int shift)
         {
             return (
-                      (currvalue & (~mask)) // Sets the bits to be changed to 0, preserving others
+                      (currvalue & (~(mask << shift))) // Sets the bits to be changed to 0, preserving others
                       | ((newvalue & mask) << shift) // Moves only the relevant bits of the new value to the relevant position, replaces the target bits
                    );  
         }
---
+++ RandomizerUnitTests/TestUtils.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+using static Randomizer.Utils;
+
+namespace RandomizerUnitTests;
+
+internal class TestUtils
+{
+    [Test]
+    public void TestSetBits()
+    {
+        byte currval1 = 0b0001;
+        byte mask1 = 0b0011;
+        byte newval1 = 0b0011;
+        byte shift1 = 1;
+        byte expected1 = 0b0111;
+        int calc1 = SetBits(currval1, newval1, mask1, shift1);
+        Assert.AreEqual(expected1, calc1, $"{expected1:x}!={calc1:x}");
+
+    }
+    [Test]
+    public void TestSetBits2() 
+    { 
+
+        byte currval1  = 0b0001;
+        byte mask1     = 0b0110;
+        byte newval1   = 0b0011;
+        byte shift1    = 0;
+        byte expected1 = 0b0011;
+        int calc1 = SetBits(currval1, newval1, mask1, shift1);
+        Assert.AreEqual(expected1, calc1, $"{expected1:x}!={calc1:x}");
+    }
+
+    [Test]
+    public void TestSetBits3()
+    {
+        byte currval1  = 0b1111;
+        byte mask1     = 0b0011;
+        byte newval1   = 0b1100;
+        byte shift1    = 1;
+        byte expected1 = 0b1001;
+        int calc1 = SetBits(currval1, newval1, mask1, shift1);
+        Assert.AreEqual(expected1, calc1, $"{expected1:x}!={calc1:x}");
+    }
+
+    [Test]
+    public void TestSetBits4()
+    {
+        byte currval1  = 0b1111;
+        byte mask1     = 0b0110;
+        byte newval1   = 0b0000;
+        byte shift1    = 0;
+        byte expected1 = 0b1001;
+        int calc1 = SetBits(currval1, newval1, mask1, shift1);
+        Assert.AreEqual(expected1, calc1, $"{expected1:x}!={calc1:x}");
+    }
+
+    [Test]
+    public void TestSetBits5()
+    {
+        byte currval1  = 0b1111_1111;
+        byte mask1     = 0b0011_1100;
+        byte newval1   = 0b0000_0000;
+        byte shift1    = 0;
+        byte expected1 = 0b1100_0011;
+        int calc1 = SetBits(currval1, newval1, mask1, shift1);
+        Assert.AreEqual(expected1, calc1, $"{expected1:x}!={calc1:x}");
+    }
+    [Test]
+    public void TestSetBits6()
+    {
+        byte currval1  = 0b1111_1111;
+        byte mask1     = 0b0000_1111;
+        byte newval1   = 0b0000_0000;
+        byte shift1    = 2;
+        byte expected1 = 0b1100_0011;
+        int calc1 = SetBits(currval1, newval1, mask1, shift1);
+        Assert.AreEqual(expected1, calc1, $"{expected1:x}!={calc1:x}");
+    }
+
+    [Test]
+    public void TestSetBits7()
+    {
+        byte currval1  = 0b1100_1110;
+        byte mask1     = 0b0011_1000;
+        byte newval1   = 0b1001_0000;
+        byte shift1    = 0;
+        byte expected1 = 0b1101_0110;
+        int calc1 = SetBits(currval1, newval1, mask1, shift1);
+        Assert.AreEqual(expected1, calc1, $"{expected1:x}!={calc1:x}");
+    }
+    [Test]
+    public void TestSetBits8()
+    {
+        byte currval1  = 0b1111_0000;
+        byte mask1     = 0b0000_1111;
+        byte newval1   = 0b0000_1010;
+        byte shift1    = 4;
+        byte expected1 = 0b1010_0000;
+        int calc1 = SetBits(currval1, newval1, mask1, shift1);
+        Assert.AreEqual(expected1, calc1, $"{expected1:x}!={calc1:x}");
+    }
+
+
+}
