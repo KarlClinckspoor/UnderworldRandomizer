547345ba582bdb1e8a4b5415a4e74f77ccbc265c
Karl Clinckspoor <30571394+KarlClinckspoor@users.noreply.github.com>
2022-08-21 19:54:20 -0300

    Added tests for Object Equality.

---
+++ RandomizerUnitTests/TestGameObjectEquality.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using NUnit.Framework;
+using Randomizer;
+namespace RandomizerUnitTests;
+
+class TestGameObjectEquality
+{
+    [Test]
+    public void TwoEqGameObjects()
+    {
+        var obj1 = new GameObject(
+            objid_flagsField: 0xFFF,
+            positionField: 0xFFF,
+            quality_chainField: 0xFFF,
+            link_specialField: 0xFFF
+        );
+
+        var obj1_copy = new GameObject(
+            objid_flagsField: 0xFFF,
+            positionField: 0xFFF,
+            quality_chainField: 0xFFF,
+            link_specialField: 0xFFF
+        );
+
+        Assert.True(obj1.Equals(obj1_copy));
+    }
+
+    [Test]
+    public void TwoDiffGameObjects()
+    {
+        var obj1 = new GameObject(
+            objid_flagsField: 0xFFF,
+            positionField: 0xFFF,
+            quality_chainField: 0xFFF,
+            link_specialField: 0xFFF
+        );
+
+        var obj2 = new GameObject(
+            objid_flagsField: 0xCCC,
+            positionField: 0xCCC,
+            quality_chainField: 0xCCC,
+            link_specialField: 0xCCC
+        );
+        
+        Assert.False(obj1.Equals(obj2));
+    }
+
+    [Test]
+    public void GOAndQtty()
+    {
+        var obj1 = new GameObject(
+            objid_flagsField: 0xFFF,
+            positionField: 0xFFF,
+            quality_chainField: 0xFFF,
+            link_specialField: 0xFFF
+        );
+        
+        var obj2 = new QuantityGameObject(
+            objid_flagsField: 0xFFF,
+            positionField: 0xFFF,
+            quality_chainField: 0xFFF,
+            link_specialField: 0xFFF
+        );
+
+        Assert.True(obj1.Equals(obj2));
+    }
+
+    [Test]
+    public void StaticAndMobileObjects()
+    {
+        var staticObj = new GameObject(
+            objid_flagsField: 0xFFF,
+            positionField: 0xFFF,
+            quality_chainField: 0xFFF,
+            link_specialField: 0xFFF
+        );
+
+        var mobileObj = new MobileObject(staticObj.Buffer, 1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+            10,
+            11,
+            12,
+            13,
+            14,
+            15
+        );
+        
+        Assert.False(staticObj.Equals(mobileObj));
+    }
+}
