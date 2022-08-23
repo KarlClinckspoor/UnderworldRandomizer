59e57b6ed6f93a1bbb1a9176a93aa0a5261baf94
Karl Clinckspoor <karl@unicamp.br>
2022-08-23 09:02:07 -0300

    Started working on extracting those binary chunks

---
+++ InfoExtraction/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using InfoExtraction;
+using Randomizer;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string arkpath;
+        string binarypath;
+        
+        if (args.Length == 0)
+        {
+            arkpath = Settings.DefaultArkPath;
+            binarypath = Settings.DefaultBinaryTestsPath;
+        }
+        else if (args.Length == 1)
+        {
+            arkpath = args[0];
+            binarypath = Settings.DefaultBinaryTestsPath;
+        }
+        else
+        {
+            arkpath = args[0];
+            binarypath = args[1];
+        }
+        Console.WriteLine(arkpath);
+        Console.WriteLine($"Loading LEV.ARK from {arkpath}");
+        var extractor = new LoadAndCreateBufferExamples(arkpath, binarypath);
+        Console.WriteLine("Saving generalized blocks");
+        // Console.WriteLine("Saving Tilemap and object data");
+        extractor.ExtractBlocks();
+    }
+}
