cb50067be5c9f5b0c42b4ce694ec5d9bdad8e387
Karl Clinckspoor <30571394+KarlClinckspoor@users.noreply.github.com>
2022-08-28 13:55:51 -0300

    Separated tests that require LEV.ARK and modified Settings.cs to not throw error if the json file isn't found. Also made it test in linux and windows.

--- RandomizerUnitTests/ArkLoaderTest.cs
+++ RandomizerUnitTests/ArkLoaderTest.cs
@@ -7,6 +7,7 @@ namespace RandomizerUnitTests;
 class ArkLoaderTest
 {
     [Test]
+    [Category("RequiresArk")]
     public void CompareLoadSerialize() 
     {
         var AL = new ArkLoader(Settings.DefaultArkPath);
