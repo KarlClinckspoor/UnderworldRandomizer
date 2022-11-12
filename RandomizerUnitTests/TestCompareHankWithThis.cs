173d94439fdd7cbece0b8322131b483a4550ce7f
Karl Clinckspoor <30571394+KarlClinckspoor@users.noreply.github.com>
2022-11-12 18:20:05 -0300

    Added test that compares hank's IDs with mine.

---
+++ RandomizerUnitTests/TestCompareHankWithThis.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using NUnit.Framework;
+using UWRandomizerEditor;
+using UWRandomizerEditor.LEVDotARK;
+using UWRandomizerEditor.LEVDotARK.GameObjects;
+
+namespace RandomizerUnitTests;
+
+[TestFixture]
+public class TestArkLoading
+{
+    [Test]
+    [Category("RequiresArk")]
+    public void TestItemIDsBlock0()
+    {
+        var path =
+            @"C:\Users\Karl\Desktop\UnderworldStudy\UnderworldRandomizer\RandomizerUnitTests\testdata\Block0_objects.json";
+        // var json = JsonSerializer.Deserialize(File.ReadAllText(path), Dictionary<string, int>);
+        var json = JsonSerializer.Deserialize<List<Dictionary<string, int>>>(File.ReadAllText(path));
+
+        var ark = new ArkLoader(Settings.DefaultArkPath);
+
+        // For whatever reason, hank's loader has objects [0-1024], but it should have been [0-1024[, right? krokot's goes
+        // goes up to 1023.
+        Assert.True(json.Count - 1 == ark.TileMapObjectsBlocks[0].AllGameObjects.Length);
+
+        for (int i = 0; i < json.Count - 1; i++)
+        {
+            var correct = json[i];
+            
+            if (i < 256)
+            {
+                var compare = ark.TileMapObjectsBlocks[0].MobileObjects[i];
+                Assert.True(correct["item_id"] == compare.ItemID);
+            }
+            else
+            {
+                var compare = ark.TileMapObjectsBlocks[0].StaticObjects[i - 256];
+                Assert.True(correct["item_id"] == compare.ItemID);
+            }
+            
+        }
+
+        // for (int i = 0)
+    }
+    
+    [Test]
+    public void TestTileInfoComparingToUltimateEditor_manual()
+    {
+        var tile = new TileInfo(1609, new byte[] {0x11, 0x20, 0x1E, 0x00}, 6436, 0);
+        // Some water tile near that island with a lurker nearby
+        var reference = new TileInfo(1609, 0, 6436, 0);
+        reference.TileType = (int) TileInfo.TileTypes.open;
+        reference.TileHeight = 1;
+        reference.DoorBit = 0;
+        reference.NoMagic = 0;
+        reference.FloorTextureIdx = 8;
+        reference.WallTextureIdx = 30;
+        reference.FirstObjIdx = 0;
+        
+
+        Assert.True(tile.Equals(reference));
+    }
+
+    // [Test]
+    // public void TestObjectFromUltimateEditor_manual()
+    // {
+    //     // // A torch to the west of the tile above
+    //     // var obj = new StaticObject(new byte[] {0x91, 0x80, 0x48, 0x34, 0x28, 0x00, 0x40, 0x00}, 540);
+    //     // var reference = new StaticObject(new byte[] {0, 0, 0, 0, 0, 0, 0, 0}, 540);
+    //     // reference.Xpos = 1;
+    //     // reference.Ypos = 5;
+    //     // reference.Zpos = 72;
+    //     // reference.IsQuant = 1;
+    //     // reference.Invis = 0;
+    //     // reference.EnchantFlag = 0;
+    //     // reference.Heading = 0;
+    //     // reference.Flags = 0;
+    //     // reference.Quality = 40;
+    //     // reference.Owner_or_special = 0;
+    //     // reference.next = 0;
+    //     // reference.ItemID = 145;
+    //     // reference.QuantityOrSpecialLinkOrSpecialProperty = 1;
+    //     // reference.link_specialField = 64;
+    //     // reference.UpdateBuffer();
+    //     //
+    //     // Assert.True(obj.Equals(reference));
+    // }
+}
