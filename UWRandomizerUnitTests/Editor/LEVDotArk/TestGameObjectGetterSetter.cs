220a4b08270975bd1afe60279b9b0a77407282ea
Karl Clinckspoor <30571394+KarlClinckspoor@users.noreply.github.com>
2023-05-15 20:03:48 -0300

    Added tests setting some MobileObject stuff.

--- UWRandomizerUnitTests/Editor/LEVDotArk/TestGameObjectGetterSetter.cs
+++ UWRandomizerUnitTests/Editor/LEVDotArk/TestGameObjectGetterSetter.cs
@@ -187,6 +187,322 @@ public class TestObjectGetterSetter
             obj.ItemOwnerStrIdx = cpy;
         });
     }
+
+    // Mobile object stuff
+    [Test]
+    public void HP()
+    {
+        IterAllObjectsAndExecuteAction(obj =>
+        {
+            if (obj is MobileObject mobj)
+            {
+                var cpy = mobj.HP;
+                mobj.HP = cpy;
+            }
+        });
+    }
+    [Test]
+    public void byte2_unk()
+    {
+        IterAllObjectsAndExecuteAction(obj =>
+        {
+            if (obj is MobileObject mobj)
+            {
+                var cpy = mobj.byte2_unk;
+                mobj.byte2_unk = cpy;
+            }
+        });
+    }
     
+    [Test]
+    public void byte3_unk()
+    {
+        IterAllObjectsAndExecuteAction(obj =>
+        {
+            if (obj is MobileObject mobj)
+            {
+                var cpy = mobj.byte3_unk;
+                mobj.byte3_unk = cpy;
+            }
+        });
+    }
+    
+    [Test]
+    public void NPCGoalGtarg()
+    {
+        IterAllObjectsAndExecuteAction(obj =>
+        {
+            if (obj is MobileObject mobj)
+            {
+                var cpy = mobj.NPCGoalGtarg;
+                mobj.NPCGoalGtarg = cpy;
+            }
+        });
+    }
+    
+    
+    [Test]
+    public void NPCLevelTalkedAttitude()
+    {
+        IterAllObjectsAndExecuteAction(obj =>
+        {
+            if (obj is MobileObject mobj)
+            {
+                var cpy = mobj.NPCLevelTalkedAttitude;
+                mobj.NPCLevelTalkedAttitude = cpy;
+            }
+        });
+    }
+    
+    [Test]
+    public void NPCheightQM()
+    {
+        IterAllObjectsAndExecuteAction(obj =>
+        {
+            if (obj is MobileObject mobj)
+            {
+                var cpy = mobj.NPCheightQM;
+                mobj.NPCheightQM = cpy;
+            }
+        });
+    }
+    
+    [Test]
+    public void byte4_unk()
+    {
+        IterAllObjectsAndExecuteAction(obj =>
+        {
+            if (obj is MobileObject mobj)
+            {
+                var cpy = mobj.byte4_unk;
+                mobj.byte4_unk = cpy;
+            }
+        });
+    }
+    
+    [Test]
+    public void byte5_unk()
+    {
+        IterAllObjectsAndExecuteAction(obj =>
+        {
+            if (obj is MobileObject mobj)
+            {
+                var cpy = mobj.byte5_unk;
+                mobj.byte5_unk = cpy;
+            }
+        });
+    }
+    
+    [Test]
+    public void byte6_unk()
+    {
+        IterAllObjectsAndExecuteAction(obj =>
+        {
+            if (obj is MobileObject mobj)
+            {
+                var cpy = mobj.byte6_unk;
+                mobj.byte6_unk = cpy;
+            }
+        });
+    }
+    
+    [Test]
+    public void byte7_unk()
+    {
+        IterAllObjectsAndExecuteAction(obj =>
+        {
+            if (obj is MobileObject mobj)
+            {
+                var cpy = mobj.byte7_unk;
+                mobj.byte7_unk = cpy;
+            }
+        });
+    }
+    
+    [Test]
+    public void byte8_unk()
+    {
+        IterAllObjectsAndExecuteAction(obj =>
+        {
+            if (obj is MobileObject mobj)
+            {
+                var cpy = mobj.byte8_unk;
+                mobj.byte8_unk = cpy;
+            }
+        });
+    }
+    
+    [Test]
+    public void NPChome()
+    {
+        IterAllObjectsAndExecuteAction(obj =>
+        {
+            if (obj is MobileObject mobj)
+            {
+                var cpy = mobj.NPChome;
+                mobj.NPChome = cpy;
+            }
+        });
+    }
+    
+    [Test]
+    public void NPCheading()
+    {
+        IterAllObjectsAndExecuteAction(obj =>
+        {
+            if (obj is MobileObject mobj)
+            {
+                var cpy = mobj.NPCHeading;
+                mobj.NPCHeading = cpy;
+            }
+        });
+    }
+    
+    [Test]
+    public void NPChunger()
+    {
+        IterAllObjectsAndExecuteAction(obj =>
+        {
+            if (obj is MobileObject mobj)
+            {
+                var cpy = mobj.byte_NPCHunger;
+                mobj.byte_NPCHunger = cpy;
+            }
+        });
+    }
+    
+    [Test]
+    public void whoami()
+    {
+        IterAllObjectsAndExecuteAction(obj =>
+        {
+            if (obj is MobileObject mobj)
+            {
+                var cpy = mobj.NPCwhoami;
+                mobj.NPCwhoami = cpy;
+            }
+        });
+    }
+    
+    [Test]
+    public void Goal()
+    {
+        IterAllObjectsAndExecuteAction(obj =>
+        {
+            if (obj is MobileObject mobj)
+            {
+                var cpy = mobj.Goal;
+                mobj.Goal = cpy;
+            }
+        });
+    }
+    [Test]
+    public void Gtarg()
+    {
+        IterAllObjectsAndExecuteAction(obj =>
+        {
+            if (obj is MobileObject mobj)
+            {
+                var cpy = mobj.Gtarg;
+                mobj.Gtarg = cpy;
+            }
+        });
+    }
+    [Test]
+    public void Level()
+    {
+        IterAllObjectsAndExecuteAction(obj =>
+        {
+            if (obj is MobileObject mobj)
+            {
+                var cpy = mobj.Level;
+                mobj.Level = cpy;
+            }
+        });
+    }
+    [Test]
+    public void TalkedTo()
+    {
+        IterAllObjectsAndExecuteAction(obj =>
+        {
+            if (obj is MobileObject mobj)
+            {
+                var cpy = mobj.TalkedTo;
+                mobj.TalkedTo = cpy;
+            }
+        });
+    }
+    [Test]
+    public void Attitude()
+    {
+        IterAllObjectsAndExecuteAction(obj =>
+        {
+            if (obj is MobileObject mobj)
+            {
+                var cpy = mobj.Attitude;
+                mobj.Attitude = cpy;
+            }
+        });
+    }
+    [Test]
+    public void Height()
+    {
+        IterAllObjectsAndExecuteAction(obj =>
+        {
+            if (obj is MobileObject mobj)
+            {
+                var cpy = mobj.Height;
+                mobj.Height = cpy;
+            }
+        });
+    }
+    [Test]
+    public void YHome()
+    {
+        IterAllObjectsAndExecuteAction(obj =>
+        {
+            if (obj is MobileObject mobj)
+            {
+                var cpy = mobj.YHome;
+                mobj.YHome = cpy;
+            }
+        });
+    }
+    [Test]
+    public void XHome()
+    {
+        IterAllObjectsAndExecuteAction(obj =>
+        {
+            if (obj is MobileObject mobj)
+            {
+                var cpy = mobj.XHome;
+                mobj.XHome = cpy;
+            }
+        });
+    }
+    [Test]
+    public void Hunger()
+    {
+        IterAllObjectsAndExecuteAction(obj =>
+        {
+            if (obj is MobileObject mobj)
+            {
+                var cpy = mobj.Hunger;
+                mobj.Hunger = cpy;
+            }
+        });
+    }
+    [Test]
+    public void whoami2()
+    {
+        IterAllObjectsAndExecuteAction(obj =>
+        {
+            if (obj is MobileObject mobj)
+            {
+                var cpy = mobj.whoami;
+                mobj.whoami = cpy;
+            }
+        });
+    }
     
 }
